bf8f63e17ff47301503fc79ca0567b4a38d2bb0a
Jay Welsh <thealchemicalopus@gmail.com>
2023-09-19 15:23:04 +0200

    cicd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,8 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Tonic.xyz Artist Guide',
@@ -11,15 +13,16 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'heretic-xyz.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tonic-artist-playbook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Heretic-xyz', // Usually your GitHub org/user name.
+  projectName: 'tonic-artist-playbook', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
